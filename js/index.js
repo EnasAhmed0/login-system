8e11394834c5128f29bcd94e8fadf5dc5ca8752a
EnasAhmed0 <enas0ahmed2@gmail.com>
2024-06-26 17:07:20 +0300

    commet part in js

--- js/index.js
+++ js/index.js
@@ -179,42 +179,42 @@ function getSignin() {
   window.location.href = './home.html';
 }
 
-function validateForm() {
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const nameError = document.getElementById('name-error');
-  const emailError = document.getElementById('email-error');
-  const passwordError = document.getElementById('password-error');
+// function validateForm() {
+//   const name = document.getElementById('name').value;
+//   const email = document.getElementById('email').value;
+//   const password = document.getElementById('password').value;
+//   const nameError = document.getElementById('name-error');
+//   const emailError = document.getElementById('email-error');
+//   const passwordError = document.getElementById('password-error');
 
 
 
-  nameError.textContent = '';
+//   nameError.textContent = '';
 
-  emailError.textContent = '';
-  passwordError.textContent = '';
-
-
-
-  let isValid = true;
-
-  if (name === '' || /\d/.test(name)) {
-    nameError.textContent = 'Please enter your name properly.';
-    isValid = false;
-  }
+//   emailError.textContent = '';
+//   passwordError.textContent = '';
 
 
-  if (email === '' || !email.includes('@')) {
-    emailError.textContent = 'Please enter a valid email address.';
-    isValid = false;
-  }
 
-  if (password === '' || password.length < 6) {
-    passwordError.textContent = 'Please enter a password with at least 6 characters.';
-    isValid = false;
-  }
+//   let isValid = true;
+
+//   if (name === '' || /\d/.test(name)) {
+//     nameError.textContent = 'Please enter your name properly.';
+//     isValid = false;
+//   }
 
 
-  return isValid;
-}
+//   if (email === '' || !email.includes('@')) {
+//     emailError.textContent = 'Please enter a valid email address.';
+//     isValid = false;
+//   }
+
+//   if (password === '' || password.length < 6) {
+//     passwordError.textContent = 'Please enter a password with at least 6 characters.';
+//     isValid = false;
+//   }
+
+
+//   return isValid;
+// }
 
